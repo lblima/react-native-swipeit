2a315d539d9b621e95041edfdf51b409b3b36b26
leonardolima <leonardo.bruno@outlook.com>
2018-08-31 12:44:42 -0300

    add animation with Animated and PanResponder

--- src/CardsContainer.js
+++ src/CardsContainer.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import { 
         View, 
         StyleSheet, 
-        Dimensions 
+        Dimensions,
+        Animated,
+        PanResponder
     } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -11,16 +13,46 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 // create a component
 class CardsContainer extends Component {
 
+    constructor(props) {
+        super(props);
+
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy });
+            },
+            onPanResponderRelease: (event, gesture) => {
+               
+            }
+        });
+
+        this.state = { panResponder, position, index: 0 };
+    }
+
     renderCards() {
         return this.props.data.map((item, index) => {
+            //Add animation only to the card on top
+            if (index === this.state.index) {
+                return (
+                    <Animated.View 
+                        style={[styles.cardStyle, this.state.position.getLayout()]}
+                        {...this.state.panResponder.panHandlers}
+                        key={item.id}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
+
             return (
-                <View 
+                <Animated.View 
                     style={styles.cardStyle}
                     key={item.id}
                 >
                     {this.props.renderCard(item)}
-                </View>
-            )
+                </Animated.View>
+            );
         }).reverse();
     }
 
